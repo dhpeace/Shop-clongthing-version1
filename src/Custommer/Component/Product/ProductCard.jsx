9f26673804d2e2d22b8aadf7b0bf839b37808bd4
tuzgbach <tungbach.tts@gmail.com>
2024-02-19 12:30:13 +0700

    fix frontend

--- src/Custommer/Component/Product/ProductCard.jsx
+++ src/Custommer/Component/Product/ProductCard.jsx
@@ -4,26 +4,27 @@ import "./Product.css";
 import PropTypes from "prop-types";
 
 function ProductCard({ product }) {
+  console.log(product);
   return (
     <div className="productCard w-[15rem] m-3 transition-all cursor-pointer">
       <div className="h-[20rem]">
         <img
           className="h-full w-full object-cover object-left-top"
-          src={product.imageUrl}
+          src={product.image}
           alt=""
         />
       </div>
       <div className="textPart bg-white p-3">
         <div>
-          <p className="font-bold opacity-40">{product.brand}</p>
+          <p className="font-bold opacity-40">{product.name}</p>
           <p className="">{product.title}</p>
         </div>
         <div className="flex items-center space-x-2">
-          <p className="font-semibold">{product.discountedPrice}</p>
-          <p className="line-through opacity-50">{product.price}</p>
-          <p className="text-green-600 font-semibold">
+          {/* <p className="font-semibold">{product.discountedPrice}</p> */}
+          <p className=" opacity-50">{product.price}</p>
+          {/* <p className="text-green-600 font-semibold">
             {product.discountPersent}
-          </p>
+          </p> */}
         </div>
       </div>
     </div>
@@ -35,11 +36,12 @@ ProductCard.propTypes = {
     id: PropTypes.number.isRequired,
     brand: PropTypes.string,
     title: PropTypes.string,
+    name: PropTypes.string, // Add this line
     discountedPrice: PropTypes.number,
     price: PropTypes.number,
     discountPercent: PropTypes.number,
-    imageUrl: PropTypes.string,
-    discountPersent: PropTypes.number, // Add this line
+    image: PropTypes.string,
+    discountPersent: PropTypes.number,
   }).isRequired,
 };
 export default ProductCard;
