ba0da4a32833f35682d443fc3d304e8ed9e65779
QuÃốc DŨng <nguyenssk043@gmail.com>
2024-02-19 13:15:28 +0700

    d-test1

--- src/Admin/Components/Admin.jsx
+++ src/Admin/Components/Admin.jsx
@@ -1,12 +1,13 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react";
+import React from "react"
 
 function Admin() {
-  return (
-    <div>
-      <h1>Admin</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Admin</h1>
+            <h1>anh da den - 1</h1>
+        </div>
+    )
 }
 
-export default Admin;
+export default Admin
