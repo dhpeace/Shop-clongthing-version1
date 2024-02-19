4499415ca26a02bb20f60249cf9146c0515bacbb
QuÃốc DŨng <nguyenssk043@gmail.com>
2024-02-19 14:11:46 +0700

    d-main-1

--- src/Admin/Components/Admin.jsx
+++ src/Admin/Components/Admin.jsx
@@ -2,30 +2,12 @@
 import React from "react"
 
 function Admin() {
-<<<<<<< HEAD
     return (
         <div>
             <h1>Admin</h1>
             <h1>anh da den - 1</h1>
         </div>
     )
-=======
-  return (
-    <div>
-      <h1>Admin</h1>
-<<<<<<< HEAD
-      <h2>wqdqw</h2>
-=======
-      {/* Code start */}
-      {/* code start 1 */}
-      {/* code end */}
-      {/* code start1 */}
-      {/* tung */}
-
->>>>>>> tungbach
-    </div>
-  );
->>>>>>> 07f952fef34941a1d298265333b5e946338fccf3
 }
 
 export default Admin
