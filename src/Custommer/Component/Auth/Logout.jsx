55131dfb3da1f604a24306308b8d4bfe5d52f161
tuzgbach <tungbach.tts@gmail.com>
2024-02-22 13:44:03 +0700

    Auth

--- src/Custommer/Component/Auth/Logout.jsx
+++ src/Custommer/Component/Auth/Logout.jsx
@@ -1,19 +1,25 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import PropTypes from "prop-types";
 
 const Logout = ({ onLogout }) => {
   let navigate = useNavigate();
 
   const handleLogout = () => {
-    onLogout(); // Call the onLogout prop
-    navigate('/login'); // Redirect to login page
-  }
+    onLogout();
+    navigate("/login");
+  };
 
   return (
-    <button onClick={handleLogout} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+    <button
+      onClick={handleLogout}
+      className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-3">
       Logout
     </button>
   );
-}
+};
+Logout.propTypes = {
+  onLogout: PropTypes.func.isRequired,
+};
 
 export default Logout;
